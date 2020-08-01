cf1cfeb14b0a31b4b6507bf46b17284f6e6e9dfc
内藤峻 <syun.naito@gmail.com>
2020-08-01 19:37:44 +0900

    add keybind and environment variable settings

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -6,15 +6,6 @@
 ;; 時間も表示
 (display-time)
 
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-;; キーバインドの設定                                     ;;
-;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
-
-;; ctrl-hをバックスペースにする
-(global-set-key "\C-h" 'delete-backward-char)
-;; ctrl-?をヘルプにする
-(global-set-key (kbd "C-?") 'help-for-help)
-
 ;; ~からはじまるバックアップファイルを作らない
 (setq make-backup-files nil)
 ;; ⌘キーをメタキーに割り当てる
@@ -75,4 +66,55 @@
 
 ;; cl-libパッケージを読み込む
 (require 'cl-lib)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; キーバインドの設定                                      ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; 文字列（"\C-m"）よりもキーシーケンス((kbd "C-m"))の視認性に優れるというメリットがある
+;; global-set-keyはdefine-key global-mapの短縮表記のようなもの
+
+;; C-mにnewline-and-indentを割り当てる。
+(global-set-key (kbd "C-m") 'newline-and-indent)
+
+;; C-hを<DEL>（バックスペース）に置き換える
+(define-key key-translation-map (kbd "C-h") (kbd "<DEL>"))
+;; 別のキーバインドにヘルプを割り当てる
+(define-key global-map (kbd "C-x ?") 'help-command)
+
+;; 折返しトグルコマンド
+(define-key global-map (kbd "C-c l") 'toggle-truncate-lines)
+
+;; "C-t" でウィンドウを切り替える。初期値はtranspose-chars
+(define-key global-map (kbd "C-t") 'other-window)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; 環境変数の設定                                         ;;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;; パスの設定
+(add-to-list 'exec-path "/usr/local/bin")
+(add-to-list 'exec-path "/opt/local/bin")
+
+;; 文字コードの指定
+(set-language-environment "Japanese")
+(prefer-coding-system 'utf-8)
+
+;; 行番号/カラム番号を表示する
+(column-number-mode t)
+
+;; 行番号を表示させない
+(line-number-mode 0)
+
+;; ファイルサイズを表示
+(size-indication-mode t)
+;; 時計を表示
+;; (setq display-time-day-and-date 0) ; 曜日・月・日を表示
+;; (setq display-time-24hr-formt 0) ; 24時間表示
+(display-time-mode t)
+;; バッテリー残量を表示
+(display-battery-mode t)
+
+;; リージョン内の行数と文字数をモードラインに表示する
+(defun count-line)
 
